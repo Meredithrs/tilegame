8716946b2ef3da48aaba49278d452f300c919dae
Meredith <meredith.xpw@gmail.com>
2014-12-20 17:34:51 -0500

    Serve a static file, maybe

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get('/', function(request, response){
 	response.send("success");
 });
 
+app.get('/game.js', function(request, response){
+	fs.openSync(__dirname + "/public/game.js", 'w');
+});
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
