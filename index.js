8559aadcfed2ff11562c72f82917337f145c4ffa
Meredith <meredith.xpw@gmail.com>
2014-12-20 18:16:55 -0500

    Fixing API setup

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-var express = require('express')
-var app = express();
+// server.js
 
+// BASE SETUP
+// =============================================================================
+
+// call the packages we need
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
 
 // configure app to use bodyParser()
@@ -8,19 +13,27 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.set('port', (process.env.PORT || 5000));
+var port = process.env.PORT || 8080;        // set our port
 
-app.use(express.compress());
-app.use(express.static(__dirname + '/public'));
-
+// ROUTES FOR OUR API
+// =============================================================================
 var router = express.Router();              // get an instance of the express Router
 
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });   
 });
 
+app.use(express.compress());
+app.use(express.static(__dirname + '/public'));
+
+// more routes for our API will happen here
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
 app.use('/api', router);
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
