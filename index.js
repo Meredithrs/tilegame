ff55291c774674b4c5b503b4e1947e31cabc1d65
Meredith <meredith.xpw@gmail.com>
2014-12-20 18:32:36 -0500

    API setup

--- index.js
+++ index.js
@@ -19,6 +19,16 @@ var port = process.env.PORT || 8080;        // set our port
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
 
+router.use(function(req, res, next) {
+    // do logging
+    console.log('Something is happening.');
+    next(); // make sure we go to the next routes and don't stop here
+});
+
+router.route('/maps').post(function(req, res){
+	res.json({"foo": "bar"});
+});
+
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });   
