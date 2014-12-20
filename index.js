735983f6fb980d0f2ee000c6e2c646beac18d0ce
Meredith <meredith.xpw@gmail.com>
2014-12-20 18:11:19 -0500

    API setup

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 var express = require('express')
 var app = express();
 
-app.set('port', (process.env.PORT || 5000))
+var bodyParser = require('body-parser');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.compress());
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response){
-	response.send("success");
+var router = express.Router();              // get an instance of the express Router
+
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });   
 });
+
+app.use('/api', router);
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
