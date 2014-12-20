1f4b786d1fe195e3dc8a95acbc08d7163f54ff97
Meredith <meredith.xpw@gmail.com>
2014-12-20 18:18:10 -0500

    Fixing API setup

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ router.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });   
 });
 
-app.use(express.compress());
-app.use(express.static(__dirname + '/public'));
+//app.use(express.compress());
+//app.use(express.static(__dirname + '/public'));
 
 // more routes for our API will happen here
 
