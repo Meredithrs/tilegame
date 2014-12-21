c9af862cef7328e02c163d1f0b9f39a4ed423137
Meredith <meredith.xpw@gmail.com>
2014-12-20 19:09:21 -0500

    Read maps

--- models/maps.js
+++ models/maps.js
@@ -4,9 +4,9 @@ module.exports	=	(function(){
 		if(x > viewport_radius && y > viewport_radius && x + viewport_radius < 100 && y + viewport_radius < 100){
 			var map 	=	require("./../maps/"+ normalize(x) + "," + normalize(y) + ".js");
 			maps.push(map);
-			maps.push({"foo", "foo"});
+			maps.push({"foo": "foo"});
 		}
-		maps.push({"foo", "foo"});
+		maps.push({"foo": "foo"});
 		return JSON.stringify(maps);
 	}
 
