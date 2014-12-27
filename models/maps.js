420f71960eaacc0dfc87722dd56f82013aee3dda
Meredith <meredith.xpw@gmail.com>
2014-12-26 21:25:48 -0500

    Make map names consistent with coordinate scheme
    Also fixed crashing when a blank map is requested

--- models/maps.js
+++ models/maps.js
@@ -2,28 +2,32 @@ module.exports	=	(function(){
 	function getJSON(x, y){
 		var maps	=	[];
 
-		var map 	=	require("./../maps/"+ normalize(x) + "," + -1*normalize(y) + ".js");
-		maps.push(map);
+		try{
+			var map 	=	require("./../maps/"+ normalize(x) + "," + 1*normalize(y) + ".js");
+			maps.push(map);
 
-		var output	=	[];
-		maps.forEach(
-			function(element, index, array){
-				var map 		=	element.layers[0].data;
-				var map_data	=	[], map_data_layer;
+			var output	=	[];
+			maps.forEach(
+				function(element, index, array){
+					var map 		=	element.layers[0].data;
+					var map_data	=	[], map_data_layer;
 
-				for(var i = 0; i < 64; i++){
-					map_data_layer	=	[];
-					for(var j = 0; j < 64; j++){
-						map_data_layer.push(map[i*64 + j]);
+					for(var i = 0; i < 64; i++){
+						map_data_layer	=	[];
+						for(var j = 0; j < 64; j++){
+							map_data_layer.push(map[i*64 + j]);
+						}
+						map_data.push(map_data_layer);
+						map_data_layer 	=	[];
 					}
-					map_data.push(map_data_layer);
-					map_data_layer 	=	[];
-				}
 
-				output	=	map_data;
-			}
-		);
-		return output;
+					output	=	map_data;
+				}
+			);
+			return output;
+		}catch(exception){
+			return [];
+		}
 	}
 
 	function normalize(coord){
